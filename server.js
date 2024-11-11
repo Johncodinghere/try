3202d63e7f4b8b9120fc60a38e71b6fb3b4e08e3
Johncodinghere <166317080+Johncodinghere@users.noreply.github.com>
2024-11-11 12:29:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(helmet());
 app.use(cors());
 app.get('/', (req, res) => {
-    res.redirect('/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Use the environment variable for MongoDB URI
